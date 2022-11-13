30f7bf312eeef53cd18bc9851d5f0803b2bae7bc
b1tg <b1tg@users.noreply.github.com>
2022-11-13 22:10:45 +0800

    support windows

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,12 @@
 // key-mgmt=wpa-psk
 // psk=testabc
 
+use encoding::all::GB18030;
+use encoding::{DecoderTrap, Encoding};
 use ini::Ini;
-use std::{
-    fmt::Formatter,
-    fs,
-    path::{Path, PathBuf},
-};
-
+use regex::Regex;
+use std::process::Command;
+use std::{fs, path::PathBuf};
 #[derive(Debug, Clone)]
 struct WifiConfig {
     ssid: String,
@@ -52,8 +51,78 @@ impl WifiConfig {
             fname: filename.to_owned(),
         })
     }
+    // netsh wlan show profiles
+    // netsh wlan show profiles name=wifi1 key=clear
+    fn fetch_win() -> Option<Vec<WifiConfig>> {
+        let mut results = vec![];
+        let output = run_command("netsh wlan show profiles")?;
+        let re = Regex::new(r".*: (.+)$").unwrap();
+        let names: Vec<&str> = output
+            .split("\r\n")
+            .filter_map(|x| {
+                let captures = re.captures(x)?;
+                if let Some(name) = captures.get(1) {
+                    return Some(name.as_str());
+                }
+                None
+            })
+            .collect();
+        for name in names {
+            let output = run_command(&format!("netsh wlan show profiles name={} key=clear", name))?;
+            let re = Regex::new(r".*关键内容.*: (.+)$").unwrap();
+            let passwords: Vec<&str> = output
+                .split("\r\n")
+                .filter_map(|x| {
+                    let captures = re.captures(x)?;
+                    if let Some(name) = captures.get(1) {
+                        return Some(name.as_str());
+                    }
+                    None
+                })
+                .collect();
+            results.push(WifiConfig {
+                ssid: name.to_string(),
+                password: passwords.get(0).or(Some(&"<EMPTY>"))?.to_string(),
+                fname: "".to_string(),
+                uuid: "".to_string(),
+            })
+        }
+        Some(results)
+    }
 }
 
+fn run_command(cmd_str: &str) -> Option<String> {
+    let cmd_str: Vec<&str> = cmd_str.trim().split_ascii_whitespace().collect();
+    let mut command = Command::new(cmd_str.get(0)?);
+    let command = command.args(cmd_str.get(1..)?);
+    let stdout = command.output().ok()?.stdout;
+    if let Ok(output) = String::from_utf8(stdout.clone()) {
+        return Some(output);
+    }
+    let output = GB18030.decode(&stdout, DecoderTrap::Strict).ok()?;
+    Some(output)
+}
+#[test]
+fn test_run_command() {
+    dbg!(run_command("netsh wlan show profiles"));
+    dbg!(run_command("whoami"));
+    assert_eq!(run_command("badbad"), None);
+}
+#[test]
+fn test_win() {
+    WifiConfig::fetch_win();
+}
+#[cfg(target_os = "windows")]
+fn main() {
+    println!("[*] searching WIFI info");
+    if let Some(entries) = WifiConfig::fetch_win() {
+        for entry in entries {
+            println!("{}: {}", entry.ssid, entry.password);
+        }
+    }
+    println!("[*] searching over, bye");
+}
+#[cfg(target_os = "linux")]
 fn main() {
     let system_connections_dir = "/etc/NetworkManager/system-connections/";
     println!("[*] searching WIFI info in {} ...", system_connections_dir);
